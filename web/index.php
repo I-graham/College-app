931a6ee9242d3bcffeaf5e1ae0dfe348f57aaf5f
Ian Graham Martinez <Ian.graham28@gmail.com>
2020-07-17 16:30:08 -0400

    redirect

--- web/index.php
+++ web/index.php
@@ -1,5 +1,14 @@
 <?php
 
-	echo file_get_contents("../templates/template.html");
+	function customError($errno, $errstr) {
+		echo "<b>Error:</b> [$errno] $errstr";
+	}
+
+	function main {
+		return file_get_contents("../templates/template.html")
+	}
+  
+	set_error_handler("customError");
+	echo main();
 
 ?>

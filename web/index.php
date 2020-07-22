eb2b9420bdfb2d9f653a962a99eba2e1bfdec79e
Ian Graham Martinez <Ian.graham28@gmail.com>
2020-07-22 12:19:07 -0400

    -

--- web/index.php
+++ web/index.php
@@ -14,11 +14,11 @@
 	$db = parse_url(getenv("DATABASE_URL"));
 
 	$pdo = new PDO("pgsql:" . sprintf(
-		"host=%s;port=%s;user=%s;password=%s;dbname=%s",
+		"host=%s;pot=%s;user=%s;password=%s;dbname=%s",
 		$db["host"],
 		$db["port"],
 		$db["user"],
-		$db["pas"],
+		$db["pass"],
 		ltrim($db["path"], "/")
 	));
 

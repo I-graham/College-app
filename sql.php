7a2fa6871957914ba6105a44d6389a6044890251
Ian Graham Martinez <Ian.graham28@gmail.com>
2020-07-23 14:30:11 -0400

    -

--- sql.php
+++ sql.php
@@ -9,19 +9,19 @@
 	
 	$output .= file_get_contents("sql.html");
 	
-	
-	$db = parse_url(getenv("DATABASE_URL"));
-	
-	$pdo = new PDO("pgsql:" . sprintf(
-		"host=%s;port=%s;user=%s;password=%s;dbname=%s",
-		$db["host"],
-		$db["port"],
-		$db["user"],
-		$db["pass"],
-		ltrim($db["path"], "/")
-	));
-	
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+		$db = parse_url(getenv("DATABASE_URL"));
+	
+		$pdo = new PDO("pgsql:" . sprintf(
+			"host=%s;port=%s;user=%s;password=%s;dbname=%s",
+			$db["host"],
+			$db["port"],
+			$db["user"],
+			$db["pass"],
+			ltrim($db["path"], "/")
+		));
+
 		if ($_REQUEST["pass"] == "foobar") {
 
 			$command = $_REQUEST["command"]

61b61ceb46f7e3fe35ac871f605deb09a2fc791b
Ian Graham Martinez <Ian.graham28@gmail.com>
2020-07-24 13:57:42 -0400

    -

--- ip.php
+++ ip.php
@@ -11,9 +11,8 @@
 		ltrim($db["path"], "/")
 	));
 
-	$ip = $_SERVER['REMOTE_ADDR'];
-	$details = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
-	echo $details->city;
+	$location = file_get_contents('http://freegeoip.net/json/'.$_SERVER['REMOTE_ADDR']);
+	print_r($location)
 
 	$prep = $con->prepare("INSERT INTO ip VALUES (?)");
 

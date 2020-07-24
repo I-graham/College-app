b7f459cf04db6ef2b1acf08cd4e0d75ab4af33b9
Ian Graham Martinez <Ian.graham28@gmail.com>
2020-07-24 14:04:24 -0400

    -

--- ip.php
+++ ip.php
@@ -1,5 +1,27 @@
 <?php 
 	
+	function get_client_ip()
+{
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP'])) {
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    } else if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else if (isset($_SERVER['HTTP_X_FORWARDED'])) {
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    } else if (isset($_SERVER['HTTP_FORWARDED_FOR'])) {
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    } else if (isset($_SERVER['HTTP_FORWARDED'])) {
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    } else if (isset($_SERVER['REMOTE_ADDR'])) {
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    } else {
+        $ipaddress = 'UNKNOWN';
+    }
+
+    return $ipaddress;
+}
+
 	$db = parse_url(getenv("DATABASE_URL"));
 
 	$con = new PDO("pgsql:" . sprintf(
@@ -13,7 +35,7 @@
 
 	$prep = $con->prepare("INSERT INTO ip VALUES (?)");
 
-	$prep->bindParam(1, $_SERVER["HTTP_CLIENT_IP"]);
+	$prep->bindParam(1, get_client_ip()]);
 
 	$prep->execute();
 

4f6e18a2eb0f6ad31348c0b5eeb257e5814d422f
Ian Graham Martinez <Ian.graham28@gmail.com>
2020-07-24 11:44:01 -0400

    Co-authored-by: ebonyyy <ebony.cumbogrant@csedge.org> Co-authored-by: adamchengnyc@gmail.com <adamchengnyc@gmail.com>

--- sql.php
+++ sql.php
@@ -1,49 +1,61 @@
-<?php
-	
-	function customError($errno, $errstr) {
-		echo "<b>Error:</b> [$errno] $errstr";
-	}
-	set_error_handler("customError");
-
-	$output = "";
-	
-	$output .= file_get_contents("sql.html");
-	
-	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-				
-		if ($_REQUEST["pass"] == "foobar") {
+<!DOCTYPE html>
+<html lang = "en-US">
+	<head>
+		<meta charset = "UTF-8">
+		<title>contact.php</title>
+		<style type = "text/css">
+			table, th, td {border: 1px solid black};
+		</style>
+	</head>
+	<body>
+		<form method="post">
+			Query : <input type="text" name="query" placeholder="query">
+			<br />
+			<input type="submit" name="submit" value="query">
+		</form>
+		<p>
+			<?php
+			if ($_SERVER["REQUEST_METHOD"] == "POST" and $_POST["pass"] == "foobar")
+				try {
+					$db = parse_url(getenv("DATABASE_URL"));
 			
-			$db = parse_url(getenv("DATABASE_URL"));
-			
-			$pdo = new PDO("pgsql:" . sprintf(
-				"host=%s;port=%s;user=%s;password=%s;dbname=%s",
-				$db["host"],
-				$db["port"],
-				$db["user"],
-				$db["pass"],
-				ltrim($db["path"], "/")
-			));
+ 					$pdo = new PDO("pgsql:" . sprintf(
+						"host=%s;port=%s;user=%s;password=%s;dbname=%s",
+						$db["host"],
+						$db["port"],
+						$db["user"],
+						$db["pass"],
+						ltrim($db["path"], "/")
+					));
 
-			$pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-			$command = $_REQUEST["command"];
-			
-			echo "Output : \n";
-
-			if ($_REQUEST["submit"] == "query") {
-				$pdo_out = $pdo->query($command);
-				foreach($pdo_out as $row) {
-					print_r($row);
-					echo "===========================\n";
-				}
-			} else if ($_REQUEST["submit"] == "query") {
-				echo $pdo->exec($command);
-			}
-
-			
-			
-		}
-	} 
-
-	echo $output;
-?>
+					$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+					
+					$query = $_REQUEST["query"];
+					//first pass just gets the column names
+					print "<table> n";
+					$result = $con->query($query);
+					//return only the first row (we only need field names)
+					$row = $result->fetch(PDO::FETCH_ASSOC);
+					print " <tr> n";
+					foreach ($row as $field => $value){
+						print " <th>$field</th> n";
+					} // end foreach
+					print " </tr> n";
+					//second query gets the data
+					$data = $con->query($query);
+					$data->setFetchMode(PDO::FETCH_ASSOC);
+					foreach($data as $row){
+						print " <tr> n";
+						foreach ($row as $name=>$value){
+							print " <td>$value</td> n";
+						} // end field loop
+						print " </tr> n";
+					} // end record loop
+					print "</table> n";
+				} catch(PDOException $e) {
+					 echo 'ERROR: ' . $e->getMessage();
+				} // end try
+			?>
+		</p>
+	</body>
+</html>

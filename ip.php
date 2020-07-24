b0aee9f3f40f970783f39241ab7c39bcc7f3a8c6
Ian Graham Martinez <Ian.graham28@gmail.com>
2020-07-24 13:57:48 -0400

    -

--- ip.php
+++ ip.php
@@ -12,7 +12,7 @@
 	));
 
 	$location = file_get_contents('http://freegeoip.net/json/'.$_SERVER['REMOTE_ADDR']);
-	print_r($location)
+	print_r($location);
 
 	$prep = $con->prepare("INSERT INTO ip VALUES (?)");
 

e2714ddf935d3caca24dcb796f3c26e1d1e97c68
Ian Graham Martinez <Ian.graham28@gmail.com>
2020-07-24 16:12:44 -0400

    -

--- web/login.php
+++ web/login.php
@@ -1,7 +1,29 @@
 <?php
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && ) {
+function signup() {
 
+}
+
+function login() {
+	
+}
+
+function redirect() {
+	header("Location: index.php");
+	exit(200);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	switch($_POST["submit"]) {	
+		case "login":
+			login();
+		case "signup":
+			signup();
+		default:
+			redirect();
+	}
+} else {
+	redirect();
 }
 
 ?>
